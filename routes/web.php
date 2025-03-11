d29985f19b2fb1d3f357bb117e24737d7743e633
erradi <erradi@email.com>
2025-03-11 23:43:13 +0100

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// routes/web.php
+
+// Route pour afficher la page de maintenance
+Route::get('/maintenance', function () {
+    return view('maintenance');
+})->name('maintenance');
